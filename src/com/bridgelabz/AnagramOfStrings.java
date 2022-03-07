d6178f9c43d1749864f7c99f30ee192f852b3cc4
ALLOJU <mounika.alloju@gmail.com>
2022-03-07 21:48:35 +0530

    UC6 with anagram of strings added

---
+++ src/com/bridgelabz/AnagramOfStrings.java
@@ -0,0 +1,51 @@
+package com.bridgelabz;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramOfStrings {
+	/**
+	 * program to calculate anagram of strings means anagram can be found by rearranging the letters 
+	 * of the words
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter first String");
+		String s1=sc.next();
+		System.out.println("Enter second String");
+		String s2=sc.next();
+		boolean isAnagram=AnagramOfStrings.isAnagram(s1,s2);
+		if(isAnagram) {
+			System.out.println("Two Strings are Anagram");
+		}
+		else {
+			System.out.println("Two Strings are Not Anagram");
+
+		}
+	}
+	/**
+	 * Purpose: method for finding two string are anagram or not
+	 * 
+	 * Identifier: P1AnagramDetection
+	 * 
+	 * @param str1 input from user
+	 * @param str2 input from user
+	 * @return true or false depending upon anagram or not
+	 */
+	private static boolean isAnagram(String s1, String s2) {
+		
+		char[] arr1 = s1.toCharArray();
+		char[] arr2 = s2.toCharArray();
+		if (s1.length() != s2.length()) {
+			return false;
+		} else {
+			Arrays.sort(arr1);
+			Arrays.sort(arr2);
+			if (Arrays.equals(arr1, arr2)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
